38f50ec04d820d1062d52ee79add1d28627483f9
Sashank576 <csashank123@gmail.com>
2025-04-22 17:01:54 +0200

    Added script for weight search experiment

--- Ludii/AnytimeSequentialHalving/src/mcts/new_experiment_scripts/run_broadentropyWeight_search.sh
+++ Ludii/AnytimeSequentialHalving/src/mcts/new_experiment_scripts/run_broadentropyWeight_search.sh
@@ -1,8 +1,6 @@
 trap '' HUP
 #Experiment to compute the best value for the entropy weight parameter in the entropy anytime SH agent    
 
-cd //home//i6317806//entropyWeight//BroadSearch
-
 #Instantiate parameters for experiment
 iteration_budget=(1000 20000 50000)
 entropy_weights=(0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0)
@@ -11,7 +9,6 @@ game_options=('"Rows/7" "Columns/7"' "" "" "")
 agents="entropyshuctanytime shuctanytime"
 
 jar_file="AgentEval.jar"
-output_directory="//home//i6317806//entropyWeight//BroadSearch"
 
 #Loop through the games, budgets and then weights
 for i in "${!games[@]}"; 
@@ -24,7 +21,7 @@ for i in "${!games[@]}";
         do
         for value in "${entropy_weights[@]}"; 
             do
-            output_folder="${output_directory}/${game_name}/budget_${budget}/weight_${value}"
+            output_folder="${game_name}//budget_${budget}//weight_${value}"
 
             mkdir -p "$output_folder"
 
