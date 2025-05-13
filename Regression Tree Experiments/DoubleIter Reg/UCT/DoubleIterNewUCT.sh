e58bba41afe6375c4f299ee0854e5f5f33f89da3
Sashank576 <csashank123@gmail.com>
2025-05-14 01:05:36 +0200

    Added Regression Tree Experiment Results

---
+++ Regression Tree Experiments/DoubleIter Reg/UCT/DoubleIterNewUCT.sh
@@ -0,0 +1,27 @@
+trap '' HUP
+#Experiment to test the double iteration regression tree agent against the standard UCT agent in multiple games with different budgets
+
+#Instantiate parameters for experiment
+iteration_budget=(1000 20000 50000)
+games=("Clobber.lud" "Breakthrough.lud" "Amazons.lud" "Yavalath.lud")
+game_options=("" "" "" "")
+agents="doubleiterregressiontreeshuctany uct"
+
+jar_file="AgentEval.jar"
+
+#Loop through the games and budgets
+for i in "${!games[@]}"; 
+    do
+    game="${games[$i]}"
+    option=${game_options[$i]}
+    game_name=$(basename "$game" .lud) 
+
+    for budget in "${iteration_budget[@]}"; 
+        do
+        output_folder="${game_name}//budget_${budget}"
+
+        mkdir -p "$output_folder"
+
+        nohup java -jar $jar_file --game "$game" --game-options $option --agents $agents --out-dir "$output_folder" --anytime-mode true --anytime-budget $budget --thinking-time -1 --iteration-limit $budget --num-games 100 --output-alpha-rank-data --output-raw-results > "${output_folder}.out" 2> "${output_folder}.err" &
+    done
+done
